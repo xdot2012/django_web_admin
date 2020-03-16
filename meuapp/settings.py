4c5def9495bb6fc5eb064016f5a3313634e08418
Douglas Sales <douglasgsales@gmail.com>
2020-03-16 14:39:57 -0300

    Django Async Setup with Celery, Flower and Rabbitmq

--- meuapp/settings.py
+++ meuapp/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'celery',
     'accounts',
+    'legacy',
+
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
