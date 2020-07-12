d7ab1cf69be2694ca4142cf76d5c6f980182a19b
Douglas <douglasgsales@gmail.com>
2020-07-12 16:33:19 -0300

    Douglas 12/07 - Django Celery Beat

--- legacy/tasks.py
+++ legacy/tasks.py
@@ -1,12 +1,15 @@
 from celery import shared_task
 import pandas as pd
 from legacy.models import Venda
+from celery.schedules import crontab
 
 @shared_task
 def task_read():
     # df = pd.read_csv(
     #     'videogamesales/relatorio_venda_IDEAL_VIP.csv', encoding='iso8859-15', sep=';')
     df = pd.read_csv('videogamesales/vgsales.csv', sep=',',  encoding='iso8859-15')
+    vendas_antigas = Venda.objects.all()
+    vendas_antigas.delete()
 
     df['Name'] = df['Name'].astype('string')
     df['Platform'] = df['Platform'].astype('string')
--- meuapp/celery.py
+++ meuapp/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meuapp.settings')
