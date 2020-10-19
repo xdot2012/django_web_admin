aca1daa965d8ca93f92445d5ae7f872aa72323c3
Douglas <douglasgsales@gmail.com>
2020-10-19 19:33:31 -0300

    correção rotas

--- meuapp/urls.py
+++ meuapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('legacy/', include('legacy.urls')),
+    path('', include('legacy.urls')),
 
 ]
