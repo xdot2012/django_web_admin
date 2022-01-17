227ecf64e7aaa4089066ef07add1f7339210bca0
douglas <douglasgsales@gmail.com>
2022-01-17 11:30:19 -0300

    Pronto

--- code/meuapp/urls.py
+++ code/meuapp/urls.py
@@ -4,11 +4,11 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('secure/auth/', include('django.contrib.auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
 
 #   User Paths
     path('accounts/', include('accounts.urls')),
-    path('internet_banking/', include('internet_banking.urls')),
+    path('secure/', include('internet_banking.urls')),
     path('', include('legacy.urls')),
 ]
